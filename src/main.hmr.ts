7c1a685e2e681ccf8736ad5fe93d7ebf11ec0694
chekhivskyiandrii <andriy12071997@gmail.com>
2020-09-20 12:00:24 +0300

    [ac] add swagger to project

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -1,11 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+import { ApplicationContext } from './app.context';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await ApplicationContext();
+
   await app.listen(3000);
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
 
   if (module.hot) {
     module.hot.accept();
