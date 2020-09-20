21336295ec6f3a40087454762734d2f2e3f7294e
Vitalii Denysiuk <dendoc97@gmail.com>
2020-09-20 13:27:11 +0300

    init pg database, first connection, create table (#2)
    * init pg database, first connection, create table
    
    * fix gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+const DB_OPTIONS: TypeOrmModuleOptions = {
+  type: 'postgres',
+  url: process.env.DB_URL,
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+  ssl: {
+    rejectUnauthorized: false
+  }
+};
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(DB_OPTIONS),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
