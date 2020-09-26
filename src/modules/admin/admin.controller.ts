2121c09541f3f5b75ec7c48f9f2e8447e9af9fbc
Andrii Chekhivskyi <andriy12071997@gmail.com>
2020-09-26 11:17:10 +0300

    add get all heads (#9)

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AdminLoginDto } from './dto/admin.login.dto';
 import { AuthService } from '../auth/auth.service';
 import { CreateHeadDto } from './dto/create.head.dto';
@@ -14,6 +14,11 @@ export class AdminController {
   @Post('login')
   login(@Body() adminLoginDto: AdminLoginDto): Promise<object> {
     return this.authService.loginAdmin(adminLoginDto);
+  }
+
+  @Get('heads')
+  getAllHeads() {
+    return this.usersService.findAllUsersHeads();
   }
 
   @Post('create/head')
