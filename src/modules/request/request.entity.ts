5fbf0e0d962bcbf5ae80d869edaa67c26506318f
Vitalii Denysiuk <dendoc97@gmail.com>
2020-10-17 11:37:36 +0300

    fix request status

--- src/modules/request/request.entity.ts
+++ src/modules/request/request.entity.ts
@@ -1,13 +1,18 @@
 import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { Theme } from 'modules/theme/theme.entity';
+import { Statuses } from './enums/statuses.enum';
 
 @Entity('request')
 export class Request {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  status: string;
+  @Column({
+    type: 'enum',
+    enum: Statuses,
+    default: Statuses.PENDING,
+  })
+  status: Statuses;
 
   @Column()
   studentId: number;
